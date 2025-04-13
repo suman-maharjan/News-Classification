f736f03d0d7562cd4fd24dee47921f3f59092e6d
nvwns1 <nvwns12345@gmail.com>
2025-04-13 22:11:52 +0545

    feat: enhance conversation saving logic with error handling and refactor news classification to use Celery worker

--- backend/src/modules/news/news.service.ts
+++ backend/src/modules/news/news.service.ts
@@ -1,4 +1,5 @@
 import axios, { AxiosInstance } from "axios";
+import celeryClient from "../../config/celery";
 import { NewsClassifySchemaType } from "./newsSchema";
 
 class NewsService {
@@ -11,16 +12,53 @@ class NewsService {
     });
   }
   async classify(payload: NewsClassifySchemaType) {
-    const { news, type } = payload;
-    let response;
-    if (type && type === "Probability") {
-      response = await this.axiosInstance.post("/classify-probability", {
-        news,
-      });
-    } else {
-      response = await this.axiosInstance.post("/classify", { news });
+    try {
+      const { news, type } = payload;
+      let response;
+      if (type && type === "Probability") {
+        response = await this.axiosInstance.post("/classify-probability", {
+          news,
+        });
+      } else {
+        response = await this.axiosInstance.post("/classify", { news });
+      }
+      const result = response.data;
+      return result;
+    } catch (error) {
+      console.error("Request failed:", error.message || error);
+      if (error.response) {
+      } else if (error.request) {
+        console.log("Request was sent but no response received");
+      } else {
+        console.log("Error setting up request:", error.message);
+      }
     }
-    const result = response.data;
+  }
+
+  // async addToJobQueue(payload: NewsClassifySchemaType) {
+  //   const { news, type } = payload;
+  //   newsQueue.add({ news, type });
+  // }
+
+  async classifyUsingWorker(payload: NewsClassifySchemaType) {
+    const { news, type } = payload;
+    const classifyTask = celeryClient.createTask("proj.tasks.classify");
+    const probabilityTask = celeryClient.createTask("proj.tasks.probability");
+
+    let result;
+
+    if (type && type === "Probability") {
+      const asyncResult = probabilityTask.applyAsync({
+        args: [news],
+        kwargs: {},
+      });
+      result = await asyncResult.get();
+      asyncResult.delete();
+    } else {
+      const asyncResult = classifyTask.applyAsync({ args: [news], kwargs: {} });
+      result = await asyncResult.get();
+      asyncResult.delete();
+    }
     return result;
   }
 }
