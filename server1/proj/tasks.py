144167a56842afaafceced9b96725fce825e5f62
nvwns1 <nvwns12345@gmail.com>
2025-04-13 22:12:09 +0545

    feat: add Celery configuration and task definitions for news classification

---
+++ server1/proj/tasks.py
@@ -0,0 +1,15 @@
+from .celery import app
+from utils.NewsClassifier import classifier
+from flask import jsonify
+
+
+@app.task
+def classify(news):
+    result = classifier.predict_news(news)
+    return {"prediction": result}
+
+
+@app.task
+def probability(news):
+    result = classifier.predict_news_probability(news)
+    return {"prediction": result}
