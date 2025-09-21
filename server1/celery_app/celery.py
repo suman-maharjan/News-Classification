3f1b3dae3ec38e50db853db758dcf5483aeb68e3
nvwns1 <nvwns12345@gmail.com>
2025-09-21 11:21:31 +0545

    refactor: remove dotenv import and loading from celery configuration

--- server1/celery_app/celery.py
+++ server1/celery_app/celery.py
@@ -1,10 +1,8 @@
 from celery import Celery
 import os
-from dotenv import load_dotenv
 from pathlib import Path
 
 env_path = Path(__file__).resolve().parent.parent / ".env"
-load_dotenv(dotenv_path=env_path)
 
 app = Celery(
     "celery_app",
