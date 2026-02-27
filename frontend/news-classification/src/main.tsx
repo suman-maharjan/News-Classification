09239332f3819138f17dda5a20d3e44cc8b823e1
Suman Maharjan <nvwns12345@gmail.com>
2026-02-27 10:40:22 +0545

    refactor: Rearrange imports and format main.tsx for improved readability

--- frontend/news-classification/src/main.tsx
+++ frontend/news-classification/src/main.tsx
@@ -1,19 +1,21 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
-import { Provider } from "react-redux";
-import store from "./redux/store.ts";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactElement, StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import App from "./App.tsx";
+import "./index.css";
+import store from "./redux/store.ts";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <Provider store={store}>
-      <QueryClientProvider client={queryClient}>
-        <App />
-      </QueryClientProvider>
-    </Provider>
-  </StrictMode>
+  (
+    <StrictMode>
+      <Provider store={store}>
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
+      </Provider>
+    </StrictMode>
+  ) as ReactElement,
 );
